dd3c7ebd06cc0a48f5625a3e440e0b9d6a87245a
cha7713 <emdaud0815!%>
2020-03-09 02:31:59 +0900

    react

---
+++ src/components/PhoneForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class PhoneForm extends Component {
+    state = {
+        name: '',
+        phone: ''
+
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) =>{
+       e.preventDfault()
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input name='name' placeholder='이름' onChange={this.handleChange} value={this.state.name} />
+                <br />
+                <input name='phone' placeholder='전화번호' onChange={this.handleChange} value={this.state.phone} />
+                
+                <div>
+                    {this.state.name}                    {this.state.phone}
+                </div>
+
+                <button type='submit'>등록</button>
+            </form>
+        )
+    }
+}
